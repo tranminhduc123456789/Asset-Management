40c71f973c03ec2dbaaa52d108e58ed247d8992f
zloystorm <saltanovd@bk.ru>
2024-06-19 16:48:38 +0300

    setup server.js

--- server.js
+++ server.js
@@ -6,23 +6,21 @@ const fs = require('fs');
 const path = require('path');
 
 const app = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
 const dbFilePath = path.join(__dirname, 'db.json');
 const adapter = process.env.NODE_ENV === 'production' ? new Memory() : new FileSync(dbFilePath);
 const db = low(adapter);
 
-// Скопируем данные из db.json в память при запуске сервера
 if (process.env.NODE_ENV === 'production') {
   const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
   db.defaults(dbData).write();
 }
 
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router(db); // Используем базу данных из lowdb напрямую
+
 app.use(middlewares);
 app.use(jsonServer.bodyParser);
 
-// Используем базу данных из памяти
 app.use((req, res, next) => {
   req.app.db = db;
   next();

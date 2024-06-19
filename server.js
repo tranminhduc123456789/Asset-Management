29fa6b81ac6946879f9c42f9f3a04e2a6ae34f6a
zloystorm <saltanovd@bk.ru>
2024-06-19 16:44:05 +0300

    setup server.js

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-const express = require('express');
 const jsonServer = require('json-server');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const Memory = require('lowdb/adapters/Memory');
+const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+
+const dbFilePath = path.join(__dirname, 'db.json');
+const adapter = process.env.NODE_ENV === 'production' ? new Memory() : new FileSync(dbFilePath);
+const db = low(adapter);
+
+// Скопируем данные из db.json в память при запуске сервера
+if (process.env.NODE_ENV === 'production') {
+  const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
+  db.defaults(dbData).write();
+}
 
 app.use(middlewares);
+app.use(jsonServer.bodyParser);
+
+// Используем базу данных из памяти
+app.use((req, res, next) => {
+  req.app.db = db;
+  next();
+});
+
 app.use(router);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running port ${port}`);
+  console.log(`JSON Server is running on port ${port}`);
 });
